6e9ab64d4a2ee34b1ba8c1e8b3c213c1a0129316
trose1237 <85703591+trose1237@users.noreply.github.com>
2021-09-22 11:00:57 -0400

    Add project files.

---
+++ studio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace studio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double radius; 
+            double pi = Math.PI;
+            double mpg;
+
+            Console.WriteLine("Enter a radius: ");
+            radius = double.Parse(Console.ReadLine());
+            double area = pi * Math.Pow(2, radius);
+            Console.WriteLine("The area of a cirlce of radius " + radius + " is " + area);
+            double circumference = 2 * pi * radius;
+            Console.WriteLine("The circumference of radius " + radius + " is " + circumference);
+            double diameter = 2 * radius;
+            Console.WriteLine("The diameter of radius " + radius + " is " + diameter);
+            Console.WriteLine("What are the MPG of your car?");
+            mpg = double.Parse(Console.ReadLine());
+            double roadTrip = circumference / mpg;
+            Console.WriteLine("You will have to travel " + roadTrip + " miles around the cirle.");
+        
+        }
+    }
+}
